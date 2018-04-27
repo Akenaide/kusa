18615cbf238a235a81d8cbd72da0eddbbaf67439
Kevin MS <kevin.mansang@mhcomm.fr>
2018-04-27 10:48:52 +0200

    Add image column

---
+++ card/static/card/main.js
@@ -0,0 +1,8 @@
+[].forEach.call(document.querySelectorAll('a[data-img-src]'), function(link) {
+    link.onmouseover = function() {
+
+        var img = link.parentElement.parentElement.lastElementChild.children[0];
+        preview = link.getAttribute("data-img-src").replace("front", "90_126");
+        img.setAttribute('src', preview);
+    };
+});
