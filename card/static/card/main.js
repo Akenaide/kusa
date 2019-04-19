42e022e41a6978355179372fa1f6e6a6b764b09a
Kevin MS <kevin.mansang@mhcomm.fr>
2019-04-19 12:17:49 +0200

    Improve mobile UI

--- card/static/card/main.js
+++ card/static/card/main.js
@@ -1,10 +1,14 @@
 [].forEach.call(document.querySelectorAll('a[data-img-src]'), function(link) {
+    var img = link.parentElement.parentElement.lastElementChild.children[0];
     link.onmouseover = function() {
-
-        var img = link.parentElement.parentElement.lastElementChild.children[0];
         preview = link.getAttribute("data-img-src").replace("front", "90_126");
         img.setAttribute('src', preview);
     };
+
+    img.parentElement.onclick = function(){
+        preview = link.getAttribute("data-img-src").replace("front", "90_126");
+        img.setAttribute('src', preview);
+    }
 });
 
 document.getElementById('toggleHelp').onclick = function() {
