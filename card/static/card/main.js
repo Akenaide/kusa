31d9ed8bb1f03536a59da4d2687c5051d336549c
Kevin MS <kevin.mansang@mhcomm.fr>
2019-06-14 11:44:50 +0200

    Kinda ugly way to not loose url params

--- card/static/card/main.js
+++ card/static/card/main.js
@@ -1,16 +1,34 @@
 [].forEach.call(document.querySelectorAll('a[data-img-src]'), function(link) {
-    var img = link.parentElement.parentElement.lastElementChild.children[0];
-    link.onmouseover = function() {
-        preview = link.getAttribute("data-img-src").replace("front", "90_126");
-        img.setAttribute('src', preview);
-    };
+  const img = link.parentElement.parentElement.lastElementChild.children[0];
+  link.onmouseover = function() {
+    preview = link.getAttribute('data-img-src').replace('front', '90_126');
+    img.setAttribute('src', preview);
+  };
 
-    img.parentElement.onclick = function(){
-        preview = link.getAttribute("data-img-src").replace("front", "90_126");
-        img.setAttribute('src', preview);
-    }
+  img.parentElement.onclick = function() {
+    preview = link.getAttribute('data-img-src').replace('front', '90_126');
+    img.setAttribute('src', preview);
+  };
 });
 
 document.getElementById('toggleHelp').onclick = function() {
-    document.getElementById('help').classList.toggle('hide');
-}
+  document.getElementById('help').classList.toggle('hide');
+};
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const url = new URL(window.location.href);
+  const sortID = new URLSearchParams(
+      document.getElementById('sort-id').getAttribute('href'));
+  const sortDiff = new URLSearchParams(
+      document.getElementById('sort-diff').getAttribute('href'));
+  for (const p of url.searchParams) {
+    sortID.set(p[0], p[1]);
+    sortDiff.set(p[0], p[1]);
+  }
+  sortID.set('sort', 'id');
+  sortDiff.set('sort', 'diff');
+  document.getElementById('sort-id').setAttribute(
+      'href', '?' + sortID.toString());
+  document.getElementById('sort-diff').setAttribute(
+      'href', '?' + sortDiff.toString());
+});
